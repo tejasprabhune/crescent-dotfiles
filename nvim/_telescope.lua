7ab96797ce5bf2fedfaa768afc7b5aaed040b912
Tejas Prabhune <tejas.prabhune@gmail.com>
2025-08-12 09:57:32 -0400

    Restore _telescope.lua

--- nvim/_telescope.lua
+++ nvim/_telescope.lua
@@ -1,4 +1,12 @@
 local builtin = require('telescope.builtin')
+require('telescope').setup{
+    defaults = {
+        file_ignore_patterns = {
+            "Pictures",
+            "Library"
+        }
+    }
+}
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fs', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") });
